3a9fc126c39f2e23249d908d09f9874c71ae1d09
Juan D Lopez <capjuancode@gmail.com>
2024-12-06 16:42:25 -0500

    update test

--- test/controllers/forecasts_controller_test.rb
+++ test/controllers/forecasts_controller_test.rb
@@ -1,48 +1,22 @@
 require "test_helper"
 
 class ForecastsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @forecast = forecasts(:one)
-  end
-
   test "should get index" do
     get forecasts_url
     assert_response :success
   end
 
-  test "should get new" do
-    get new_forecast_url
-    assert_response :success
-  end
-
-  test "should create forecast" do
-    assert_difference("Forecast.count") do
-      post forecasts_url, params: { forecast: {  } }
-    end
-
-    assert_redirected_to forecast_url(Forecast.last)
-  end
-
-  test "should show forecast" do
-    get forecast_url(@forecast)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_forecast_url(@forecast)
-    assert_response :success
-  end
-
-  test "should update forecast" do
-    patch forecast_url(@forecast), params: { forecast: {  } }
-    assert_redirected_to forecast_url(@forecast)
-  end
-
-  test "should destroy forecast" do
-    assert_difference("Forecast.count", -1) do
-      delete forecast_url(@forecast)
-    end
-
-    assert_redirected_to forecasts_url
+  test "should renders index with forecasts" do
+    params = {
+      forecast: {
+      address: "Miami, FL, USA",
+      latitude: '25.7616798', 
+      longitude: '-80.1917902'
+    }}
+    
+    get forecasts_url, xhr: true, params: params.merge(format: :html)
+    
+    assert_includes @response.body, "forecast-expanded"
+    assert_match @controller.view_assigns['forecasts'].first['summary'], @response.body
   end
 end
