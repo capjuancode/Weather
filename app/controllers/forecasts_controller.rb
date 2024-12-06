5e6b825d3ea5e36b6a455d4380d2fee167664cf9
Juan Lopez <capjuancode@gmail.com>
2024-12-06 14:10:13 -0500

    strong params and cache

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,73 +1,32 @@
 class ForecastsController < ApplicationController
-  before_action :set_forecast, only: %i[ show edit update destroy ]
+  before_action :set_forecasts, only: %i[index]
 
   # GET /forecasts or /forecasts.json
-  def index
-    data = []
-    data = OpenWeather::Client.new.one_call(lat: params['latitude'], lon: params['longitude'])['daily'] if params['latitude']&.present?
-
-    @forecasts = data
-  end
+  # set_forecast is called
+  def index; end
 
   # GET /forecasts/1 or /forecasts/1.json
-  def show
-  end
-
-  # GET /forecasts/new
-  def new
-    @forecast = Forecast.new
-  end
-
-  # GET /forecasts/1/edit
-  def edit
-  end
-
-  # POST /forecasts or /forecasts.json
-  def create
-    @forecast = Forecast.new(forecast_params)
-
-    respond_to do |format|
-      if @forecast.save
-        format.html { redirect_to @forecast, notice: "Forecast was successfully created." }
-        format.json { render :show, status: :created, location: @forecast }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @forecast.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /forecasts/1 or /forecasts/1.json
-  def update
-    respond_to do |format|
-      if @forecast.update(forecast_params)
-        format.html { redirect_to @forecast, notice: "Forecast was successfully updated." }
-        format.json { render :show, status: :ok, location: @forecast }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @forecast.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /forecasts/1 or /forecasts/1.json
-  def destroy
-    @forecast.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to forecasts_path, status: :see_other, notice: "Forecast was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
+  def show; end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_forecast
-      @forecast = Forecast.find(params[:id])
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_forecasts
+    if forecast_params['address'].present?
+      @forecasts = Rails.cache.fetch(forecast_params['address'], expires_in: 30.minutes) do
+        if forecast_params['latitude']&.present?
+          OpenWeather::Client.new.one_call(lat: forecast_params['latitude'], lon: forecast_params['longitude'])['daily'] 
+        end
+      end
     end
 
-    # Only allow a list of trusted parameters through.
-    def forecast_params
-      params.fetch(:forecast, {})
-    end
+    @forecasts ||= []
+  end
+
+  # Only allow a list of trusted parameters through.
+  def forecast_params
+    return {} unless params['forecast'].present?
+
+    params.fetch(:forecast).permit(:address, :latitude, :longitude)
+  end
 end
